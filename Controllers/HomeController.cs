f4d293c60e4ade5e02947e98b0cbb36e3f3f5b45
markes41 <marcosemmanuelsequeira@outlook.com>
2020-11-19 20:04:36 -0300

    Cambios en css en index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,8 @@ namespace DigitalScilicet.Controllers
             return View();
         }
 
+        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
